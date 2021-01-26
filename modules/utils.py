f0e44f5f575b786bdc3dfda950c96d3d91a4d2c4
LamNguy <lamchipabc@gmail.com>
2021-01-26 07:02:03 -0500

    v1.1

--- modules/utils.py
+++ modules/utils.py
@@ -1,8 +1,16 @@
 from modules.vm import *
 class Utils:
     def __init__ (self, conn):
-        self.conn = conn 
-
+        self.conn = conn.connection()
+        self.instance_types_service = self.conn.system_service().instance_types_service()
+        self.disk_service = self.conn.system_service().disks_service()
+        self.vms_service = self.conn.system_service().vms_service()
+        
     def create_vm (self, vm):
         print(self.conn)
         vm.test()
+        virtual_machine = self.vms_service.list(search='{}'.format(vm.name))[0]
+        print(virtual_machine.name)
+	disks = self.disk_service.list()
+	for disk in disks:
+		print(disk.name)
